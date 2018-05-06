45e4e1a833db106f22dbdce5852133f8f9e43664
Haitham Khedr <Haithamkhedr@gmail.com>
2018-05-06 12:08:49 +0200

    Initial Commit

---
+++ Data Structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structures
+//
+//  Created by Haitham Khedr on 5/6/18.
+//  Copyright © 2018 Haitham Khedr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
