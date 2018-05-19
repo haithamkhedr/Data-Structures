7897fd0ef8a82cf163dd3366575be8441a1ed1dd
Haitham Khedr <Haithamkhedr@gmail.com>
2018-05-19 15:43:14 +0200

    Implemented Heapify

--- Week2/make_heap/build_heap.cpp
+++ Week2/make_heap/build_heap.cpp
@@ -13,13 +13,18 @@ class HeapBuilder {
  private:
   vector<int> data_;
   vector< pair<int, int> > swaps_;
-
   void WriteResponse() const {
     cout << swaps_.size() << "\n";
     for (int i = 0; i < swaps_.size(); ++i) {
       cout << swaps_[i].first << " " << swaps_[i].second << "\n";
     }
   }
+    void writeHeap() const{
+        for (int i = 0; i < data_.size(); ++i) {
+            cout << data_[i] << " ";
+        }
+        cout<< std::endl;
+    }
 
   void ReadData() {
     int n;
@@ -28,7 +33,34 @@ class HeapBuilder {
     for(int i = 0; i < n; ++i)
       cin >> data_[i];
   }
-
+    int parent(int i){
+        return (i-1)/2;
+    }
+    int leftChild(int i){
+        return 2*i+1 > data_.size() - 1 ? i: 2*i+1;
+        
+    }
+    int rightChild(int i){
+        return 2*i+2 > data_.size() - 1 ? i: 2*i+2;
+    }
+    void siftDown(int i){
+        int l_child = leftChild(i);
+        int r_child = rightChild(i);
+        if((data_[i] <= data_[l_child] && data_[i] <= data_[r_child])){
+            return;
+        }
+        else{
+            int swap_idx = l_child;
+            if(data_[r_child] < data_[l_child]){
+                swap_idx = r_child;
+            }
+            if(swap_idx != i){
+                swap(data_[i], data_[swap_idx]);
+                swaps_.push_back(make_pair(i, swap_idx));
+                siftDown(swap_idx);
+            }
+        }
+    }
   void GenerateSwaps() {
     swaps_.clear();
     // The following naive implementation just sorts 
@@ -38,12 +70,9 @@ class HeapBuilder {
     // but in the worst case gives a quadratic number of swaps.
     //
     // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
-        }
+      int lastParent = parent(data_.size() -1 );
+      for(int i = lastParent; i >= 0; --i){
+          siftDown(i);
       }
   }
 
@@ -52,9 +81,9 @@ class HeapBuilder {
     ReadData();
     GenerateSwaps();
     WriteResponse();
+      //writeHeap();
   }
 };
-
 int main() {
   std::ios_base::sync_with_stdio(false);
   HeapBuilder heap_builder;
