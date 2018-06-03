728bbc69d4a50d33bbd8ba3f140447b03475a44d
Haitham Khedr <Haithamkhedr@gmail.com>
2018-06-03 03:04:38 +0200

    Implemented Assignment 3.3

--- Week4/hash_substring/hash_substring.cpp
+++ Week4/hash_substring/hash_substring.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <math.h>
 
 using std::string;
+using std::vector;
 typedef unsigned long long ull;
 
 struct Data {
@@ -21,12 +23,50 @@ void print_occurrences(const std::vector<int>& output) {
     std::cout << "\n";
 }
 
+ull hash_fn(const string & str, int l, int r,const ull x= 263,const ull p =1000000007 ){
+    ull hash = 0;
+    for(int i = r; i >=l; --i){
+        hash = (hash * x + str[i]) % p;
+    }
+    return hash;
+}
+vector<ull> preComputeHashes(const string &str, int P){
+    static const ull x = 263;
+    static const ull prime = 1000000007;
+    ull xP = 1 ;
+    for (int i = 0; i < P; ++i) {
+        xP = (xP * x) % prime;
+    }
+    vector<ull> hashes(str.size() - P + 1);
+    hashes.back() = hash_fn(str,(int)str.size() - P, (int)str.size() - 1,x,prime);
+    for(int i = (int)(str.size() - P -1); i >=0; --i){
+        long long hash = ((hashes[i+1] * x) % prime + str[i] - (str[i + P] * xP) % prime);
+        hash = hash + prime;
+        hashes[i] = hash % prime ;
+    }
+    return hashes;
+}
 std::vector<int> get_occurrences(const Data& input) {
     const string& s = input.pattern, t = input.text;
+    int P =(int) s.length();
+    ull pHash = hash_fn(s,0,P - 1);
+    vector<ull> tHashes = preComputeHashes(t,P);
     std::vector<int> ans;
-    for (size_t i = 0; i + s.size() <= t.size(); ++i)
-        if (t.substr(i, s.size()) == s)
-            ans.push_back(i);
+    for (int i = 0; i < (int)tHashes.size(); ++i) {
+        if(pHash != tHashes[i])
+            continue;
+        else{
+            bool match = true;
+            for(int j = i; j < i+P ; ++j){
+                if(t[j] != s[j-i]){
+                    match = false;
+                }
+            }
+            if(match){
+                ans.push_back(i);
+            }
+        }
+    }
     return ans;
 }
 
